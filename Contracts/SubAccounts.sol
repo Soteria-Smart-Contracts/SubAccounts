168933c01b70995b2582a77a35188dcb9d8f0266
Darius Tanz <scratchticketseb@gmail.com>
2023-10-04 13:02:49 +0000

    Oct 4, 2023, 9:02 AM

--- Contracts/SubAccounts.sol
+++ Contracts/SubAccounts.sol
@@ -5,9 +5,5 @@ contract SubAccounts{
     mapping(address => uint256[]) public AddressSubAccounts;
     mapping(uint256 => address) public SubAccountOwner;
 
-    struct SubAccount{
-        uint256 ID;
-        string NickName;
-        uint256 balance;
-    }
+    
 }

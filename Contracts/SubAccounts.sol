b9d0369fdbabd65017a5a134938057923de669dd
Darius Tanz <scratchticketseb@gmail.com>
2023-10-04 14:36:33 +0000

    Oct 4, 2023, 10:36 AM

--- Contracts/SubAccounts.sol
+++ Contracts/SubAccounts.sol
@@ -23,7 +23,7 @@ contract SubAccounts{
         SubIDIncrement++;
 
         address NewSubAccountAddress = address(new SubAccount(msg.sender, Nickname, NewSubAccountID));
-        SubAccountAddress[NewSubAccountID] = NewSubAccountAddressSubAccountAddress
+        SubAccountAddress[NewSubAccountID] = NewSubAccountAddress;
         SubAccountOwner[NewSubAccountID] = msg.sender;
         AddRemoveSubAccount(NewSubAccountID, true);
 

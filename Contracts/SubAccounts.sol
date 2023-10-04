c19ff8aa4b7768b900198a60a06b2d105d0e38be
Darius Tanz <scratchticketseb@gmail.com>
2023-10-04 13:07:30 +0000

    Oct 4, 2023, 9:07 AM

--- Contracts/SubAccounts.sol
+++ Contracts/SubAccounts.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.19;
 
 contract SubAccounts{
-    
+
     mapping(address => uint256[]) public AddressSubAccounts;
     mapping(uint256 => address) public SubAccountOwner;
 
@@ -18,19 +18,6 @@ contract SubAccount{
         SubAccountID = _SubAccountID;
     }
 
-    function deposit(uint256 _amount) public{
-        balance += _amount;
-    }
-    function withdraw(uint256 _amount) public{
-        balance -= _amount;
-    }
-    function getBalance() public view returns(uint256){
-        return balance;
-    }
-    function getOwner() public view returns(address){
-        return owner;
-    }
-    function getSubAccountID() public view returns(uint256){
-        return SubAccountID;
+    receive() external payable{
     }
 }

b326fe79a8cd74a91c503c142ba1d6a2ac614dff
Darius Tanz <scratchticketseb@gmail.com>
2023-10-04 13:02:24 +0000

    Oct 4, 2023, 9:02 AM

--- Contracts/SubAccounts.sol
+++ Contracts/SubAccounts.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.19;
 
 contract SubAccounts{
-    mapping(address => uint256[]) public SubAccounts;
+    mapping(address => uint256[]) public AddressSubAccounts;
     mapping(uint256 => address) public SubAccountOwner;
+
+    str
 }

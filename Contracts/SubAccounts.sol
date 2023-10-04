b12b251a569991626b04d138ea7a616ad015ed83
Darius Tanz <scratchticketseb@gmail.com>
2023-10-04 14:21:12 +0000

    Oct 4, 2023, 10:21 AM

--- Contracts/SubAccounts.sol
+++ Contracts/SubAccounts.sol
@@ -53,7 +53,7 @@ contract SubAccounts{
     
     function AddRemoveSubAccount(uint256 ID, bool Addremove) internal {
         if(Addremove){
-            
+            AddressSubAccounts[msg.sender].push(NewSubAccountID);
         }
     }
 
